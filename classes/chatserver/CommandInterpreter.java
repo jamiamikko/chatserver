8a26f4660a106b23d391b52a841f746bcf7ccde9
tundet <ryunde.taba@gmail.com>
2016-09-22 22:33:14 +0300

    Update CommandInterpreter.java

--- classes/chatserver/CommandInterpreter.java
+++ classes/chatserver/CommandInterpreter.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-/**
- *
- * @author jamiamikko
- */
 public class CommandInterpreter implements Runnable {
 
     Scanner reader = new Scanner(System.in);
@@ -24,6 +15,8 @@ public class CommandInterpreter implements Runnable {
         String username = "";
 
         System.out.println("Hello!");
+
+        ChatHistory history = ChatHistory.getInstance();
 
         while (true) {
 
@@ -48,7 +41,7 @@ public class CommandInterpreter implements Runnable {
                     System.exit(0);
 
                 case ":history":
-                    System.out.println(ChatHistory.getInstance());
+                    System.out.print(history.toString());
                 default:
                     if (username.isEmpty()) {
                         System.out.println("Username not set.");
@@ -59,11 +52,13 @@ public class CommandInterpreter implements Runnable {
                         System.out.println("Username is " + username);
                     } else {
                         System.out.println(username + ": " + command);
+                        ChatMessage chatmessage = new ChatMessage(username, command);
+
+                        history.insert(chatmessage);
 
                     }
             }
 
-            
         }
     }
 
