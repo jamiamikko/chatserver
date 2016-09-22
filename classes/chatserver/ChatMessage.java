b359ff6967abb70eae4c5ea21696f0ffb85e92d0
tundet <ryunde.taba@gmail.com>
2016-09-22 22:31:58 +0300

    Update ChatMessage.java

--- classes/chatserver/ChatMessage.java
+++ classes/chatserver/ChatMessage.java
@@ -1,26 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author jamiamikko
- */
 public class ChatMessage {
-    private String message;
-    private String username;
-    
+
+    String message;
+    String username;
+
     ChatMessage(String username, String message) {
         this.username = username;
         this.message = message;
     }
 
-    
-    public String toString() {
-    
-        return "";
-    }
-    
 }
