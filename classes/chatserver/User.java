5391ee1696ff5b1ffdc15923b4d2d561d0324961
tundet <ryunde.taba@gmail.com>
2016-09-27 20:07:55 +0300

    variables no longer private

--- classes/chatserver/User.java
+++ classes/chatserver/User.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Otto
- */
 public class User implements ChatObserver {
 
     /*Instace variables for chatObserver*/
-    private String name;
-    private String status;
+    String name;
+    String status;
 
     /*Constructor for User*/
     public User(String name) {
